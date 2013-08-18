a66c39157d02acd48253f15b291546e4843cdbd6
Charlie Key <ckey@paranoidferret.com>
2013-08-17 23:07:38 -0400

    updated faqs to pull from data and updated template

---
+++ client/faqs.js
@@ -0,0 +1,34 @@
+if(Meteor.isClient) {
+  Template.faqs.faqs = function () {
+    var data = new Array();
+    data.push({
+      question : 'What kind of cards do I receive?',
+      answer : 'You’ll recieve 1/3 of your packs from the latest core set (currently 2014) and 2/3 from other current sets (currently ravinca ...).'
+    });
+    data.push({
+      question : 'What kind of cards do I receive?',
+      answer : 'You’ll recieve 1/3 of your packs from the latest core set (currently 2014) and 2/3 from other current sets (currently ravinca ...).'
+    });
+    data.push({
+      question : 'What kind of cards do I receive?',
+      answer : 'You’ll recieve 1/3 of your packs from the latest core set (currently 2014) and 2/3 from other current sets (currently ravinca ...).'
+    });
+    data.push({
+      question : 'What kind of cards do I receive?',
+      answer : 'You’ll recieve 1/3 of your packs from the latest core set (currently 2014) and 2/3 from other current sets (currently ravinca ...).'
+    });
+    data.push({
+      question : 'What kind of cards do I receive?',
+      answer : 'You’ll recieve 1/3 of your packs from the latest core set (currently 2014) and 2/3 from other current sets (currently ravinca ...).'
+    });
+    data.push({
+      question : 'What kind of cards do I receive?',
+      answer : 'You’ll recieve 1/3 of your packs from the latest core set (currently 2014) and 2/3 from other current sets (currently ravinca ...).'
+    });
+    data.push({
+      question : 'What kind of cards do I receive?',
+      answer : 'You’ll recieve 1/3 of your packs from the latest core set (currently 2014) and 2/3 from other current sets (currently ravinca ...).'
+    });
+    return data;
+  };
+}
